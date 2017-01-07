eb9ae64c53c6b4b66b70a5eef205f19bc2d0a0b5
masterUng <ubc1515@hotmail.com>
2017-01-07 13:04:27 +0700

    after lunch

--- app/src/main/java/com/pnsat/learnsql/MainActivity.java
+++ app/src/main/java/com/pnsat/learnsql/MainActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //explicit
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    }  //Main Method นี่คือ เมธอดหลัก
+} // Main Class
