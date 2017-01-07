8b5bf21a68d1d837b6df00e9eaae60b37021d1ba
masterUng <ubc1515@hotmail.com>
2017-01-07 14:15:33 +0700

    after lunch

--- app/src/main/java/com/pnsat/learnsql/MainActivity.java
+++ app/src/main/java/com/pnsat/learnsql/MainActivity.java
@@ -2,10 +2,13 @@ package com.pnsat.learnsql;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     //explicit
+
+    private Button signInButton, signUpButton;
 
 
 
@@ -14,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindwidget();
+
+
     }  //Main Method นี่คือ เมธอดหลัก
+
+    //คือการผูก
+
+
+
+    private void bindwidget() {
+
+
+    }
 } // Main Class
