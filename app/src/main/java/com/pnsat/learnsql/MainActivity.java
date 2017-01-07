afa71fc8e752a816c2a6d5ca477c299d913e40dc
masterUng <ubc1515@hotmail.com>
2017-01-07 14:39:21 +0700

    new class

--- app/src/main/java/com/pnsat/learnsql/MainActivity.java
+++ app/src/main/java/com/pnsat/learnsql/MainActivity.java
@@ -1,7 +1,9 @@
 package com.pnsat.learnsql;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +31,23 @@ public class MainActivity extends AppCompatActivity {
 
     private void bindwidget() {
 
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+        buttomController();
+
 
     }
+
+    private void buttomController() {
+        // for signUp
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUp.class));
+
+            }
+        });
+
+
+    } // buttomController
 } // Main Class
