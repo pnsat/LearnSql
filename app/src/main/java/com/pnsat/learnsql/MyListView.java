64b2bfcd1397a887a5ce608fa440b3fec59e4b67
masterUng <ubc1515@hotmail.com>
2017-01-08 15:07:01 +0700

    Finish 8 Jan

--- app/src/main/java/com/pnsat/learnsql/MyListView.java
+++ app/src/main/java/com/pnsat/learnsql/MyListView.java
@@ -1,7 +1,10 @@
 package com.pnsat.learnsql;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MyListView extends AppCompatActivity {
@@ -28,7 +31,19 @@ public class MyListView extends AppCompatActivity {
     } // Main Method
 
     private void createListView() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM userTABLE", null);
+        cursor.moveToFirst();
+        String[] nameStrings = new String[cursor.getCount()];
+        for (int i=0;i<nameStrings.length;i++) {
+            nameStrings[i] = cursor.getString(1);
+            cursor.moveToNext();
 
+        }
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<String>(MyListView.this,
+                android.R.layout.simple_list_item_1, nameStrings);
+        listView.setAdapter(stringArrayAdapter);
     }
 
 
