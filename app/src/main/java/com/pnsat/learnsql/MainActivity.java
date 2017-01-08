2fe695889c90acbde17fc12e3afa24e13d0856d2
masterUng <ubc1515@hotmail.com>
2017-01-08 13:52:57 +0700

    Check Space Ok

--- app/src/main/java/com/pnsat/learnsql/MainActivity.java
+++ app/src/main/java/com/pnsat/learnsql/MainActivity.java
@@ -1,10 +1,18 @@
 package com.pnsat.learnsql;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +20,18 @@ public class MainActivity extends AppCompatActivity {
 
     private Button signInButton, signUpButton;
     private MyManage myManage;
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
 
 
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -28,16 +45,24 @@ public class MainActivity extends AppCompatActivity {
         bindwidget();
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }  //Main Method นี่คือ เมธอดหลัก
 
     //คือการผูก
-
 
 
     private void bindwidget() {
 
         signInButton = (Button) findViewById(R.id.button);
         signUpButton = (Button) findViewById(R.id.button2);
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText6);
+
+
+
+
         buttomController();
 
 
@@ -53,6 +78,70 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //For singIN
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAuthen();
+            }
+        });
+
 
     } // buttomController
+
+    private void checkAuthen() {
+
+        // Get value
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        if (userString.equals("") || passwordString.equals("")) {
+            // Have Space
+            myDialog(getResources().getString(R.string.haveSpace));
+        }
+
+
+
+
+    }  // checkAuthen
+
+    private void myDialog(String string) {
+        Toast.makeText(MainActivity.this, string, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 } // Main Class
